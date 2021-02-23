ab2bc67e10cf9ec5f5b36170352577d8d7a87e54
Abkan67 <78571678+Abkan67@users.noreply.github.com>
2021-02-23 10:04:25 -0500

    Delete endpoints.js

--- endpoints.js
+++
@@ -1,5 +0,0 @@
-function Endpoint(x,y) {
-  this.x=x;this.y=y;this.rad=3;
-  this.update = function() {this.draw();}
-  this.draw = function() {c.fillStyle="#FF0"; c.beginPath(); c.arc(this.x,this.y,this.rad,0,Math.PI*2); c.fill();}
-}
